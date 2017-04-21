b0cb21b73c8904aa6d622cc87313c29862877781
firasBarrek <mohamedfiras.barrek@esprit.tn>
2017-04-21 12:16:45 +0100

    converting

--- java-source/src/main/java/com/template/api/IssuerApi.java
+++ java-source/src/main/java/com/template/api/IssuerApi.java
@@ -176,8 +176,8 @@ public class IssuerApi {
     }
 
     @GET
-    @Path("converting/{peer}/{money}/from/{currency1}/to/{currency2}")
-    public String converting(@PathParam("peer") String peer, @PathParam("money") double money,
+    @Path("converting/{money}/from/{currency1}/to/{currency2}")
+    public long converting(@PathParam("money") double money,
                              @PathParam("currency1") String c1, @PathParam("currency2") String c2) {
 
         if (this.isConverter(this.me)) {
@@ -187,35 +187,36 @@ public class IssuerApi {
                     updateNotaries();
                 }
 
-                Party party = services.partyFromName(peer);
-                Currency curr = ContractsDSL.USD;
+                //Party party = services.partyFromName(peer);
+                //Currency curr = ContractsDSL.USD;
 
                 double ratio = convert(c1,c2);
                 System.out.println("ration finale = "+ratio);
                 money = money * ratio ;
 
-                if(c2.equals("USD"))
+               /* if(c2.equals("USD"))
                     curr = ContractsDSL.USD;
                 else if(c2.equals("EUR"))
                     curr = ContractsDSL.EUR;
                 else if(c2.equals("CHF"))
                     curr = ContractsDSL.CHF;
                 else if(c2.equals("GBP"))
-                    curr = ContractsDSL.GBP;
+                    curr = ContractsDSL.GBP;*/
 
-                System.out.println("money = "+money+ " curr = "+curr);
+                //System.out.println("money = "+money+ " curr = "+curr);
 
-                CashFlowCommand.IssueCash cash = new CashFlowCommand.IssueCash(new Amount<>((long) money, curr), OpaqueBytes.Companion.of((byte) 1), party, notaries.get(0));
+                /*CashFlowCommand.IssueCash cash = new CashFlowCommand.IssueCash(new Amount<>((long) money, curr), OpaqueBytes.Companion.of((byte) 1), party, notaries.get(0));
                 FlowHandle handle = services.startFlowDynamic(IssuerFlow.IssuanceRequester.class, cash.getAmount(), cash.getRecipient(), cash.getIssueRef(), services.nodeIdentity().getLegalIdentity());
 
                 SignedTransaction signedTransaction = (SignedTransaction) handle.getReturnValue().get();
-                return signedTransaction.getId().toString();
+                return signedTransaction.getId().toString();*/
+				return (long) money;
             } catch (Exception e) {
                 e.printStackTrace();
-                return e.getMessage();
+                return -1;
             }
         } else {
-            return "Not An Converter";
+            return -1;
         }
     }
 
